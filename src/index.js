b8c2408b06836522bf2bcf5be48975846b5125fa
faridrizki3377-debug <faridrizki3377@gmail.com>
2026-02-20 19:55:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/transaction', require('./routes/transactionRoutes'));
+app.use('/ppob', require('./routes/ppobRoutes'));
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Server running...')
+);

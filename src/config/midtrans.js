22b50ad96de97629bd38d6cc7967df8433edb171
faridrizki3377-debug <faridrizki3377@gmail.com>
2026-02-20 19:54:03 +0700

    Update midtrans.js

--- src/config/midtrans.js
+++ src/config/midtrans.js
@@ -1,1 +1,8 @@
+const midtransClient = require('midtrans-client');
 
+let snap = new midtransClient.Snap({
+  isProduction: true,
+  serverKey: process.env.MIDTRANS_SERVER_KEY
+});
+
+module.exports = snap;

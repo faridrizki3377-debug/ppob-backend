043668ee6379a56b120d8bb3d758adb719840f39
faridrizki3377-debug <faridrizki3377@gmail.com>
2026-02-20 19:54:38 +0700

    Update transactionController.js

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,1 +1,15 @@
+const snap = require('../config/midtrans');
 
+exports.createPayment = async (req, res) => {
+  const { amount } = req.body;
+
+  const parameter = {
+    transaction_details: {
+      order_id: 'ORDER-' + Date.now(),
+      gross_amount: amount
+    }
+  };
+
+  const transaction = await snap.createTransaction(parameter);
+  res.json({ token: transaction.token });
+};

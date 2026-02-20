be43d4e1a3be5218aea6714d293b8dbd4ee103cf
faridrizki3377-debug <faridrizki3377@gmail.com>
2026-02-20 19:55:14 +0700

    Update ppobController.js

--- src/controllers/ppobController.js
+++ src/controllers/ppobController.js
@@ -1,1 +1,16 @@
+const axios = require('axios');
+require('dotenv').config();
 
+exports.topupPulsa = async (req, res) => {
+  const { phone, code } = req.body;
+
+  const response = await axios.post('https://api.digiflazz.com/v1/transaction', {
+    username: process.env.PPOB_USERNAME,
+    buyer_sku_code: code,
+    customer_no: phone,
+    ref_id: 'REF' + Date.now(),
+    sign: process.env.PPOB_SIGNATURE
+  });
+
+  res.json(response.data);
+};

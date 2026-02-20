8f931984a78b4954e33eaff9afd3f4ef0b07915b
faridrizki3377-debug <faridrizki3377@gmail.com>
2026-02-20 20:04:12 +0700

    Update webhookController.js

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -1,1 +1,28 @@
+const pool = require('../config/db');
 
+exports.midtransWebhook = async (req, res) => {
+  const { order_id, transaction_status, gross_amount } = req.body;
+
+  if (transaction_status === "settlement") {
+    const trx = await pool.query(
+      "SELECT * FROM transactions WHERE order_id=$1",
+      [order_id]
+    );
+
+    if (!trx.rows.length) return res.status(404).json({ msg: "Not found" });
+
+    const userId = trx.rows[0].user_id;
+
+    await pool.query(
+      "UPDATE users SET balance = balance + $1 WHERE id=$2",
+      [gross_amount, userId]
+    );
+
+    await pool.query(
+      "UPDATE transactions SET status='SUCCESS' WHERE order_id=$1",
+      [order_id]
+    );
+  }
+
+  res.json({ msg: "Webhook processed" });
+};
